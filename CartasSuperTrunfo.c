e5e5632f9c602a98371e7d038430fdddd2abcfe7
29-Breno <brenofreis69@gmail.com>
2025-03-28 13:44:00 +0000

    Nivel Mestre

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -12,7 +12,7 @@ int main() {
         int populacao;
         float area;
         float pib;
-        int pontosTuristicos;
+        unsigned long int pontosTuristicos;
     };
 
     struct Pais pais1, pais2;
@@ -32,7 +32,7 @@ int main() {
     fgets(pais1.cidade, sizeof(pais1.cidade), stdin);
 
     printf("digite o numero de habitantes da cidade '.'Nao usar para separar centenas: \n");
-    scanf("%d", &pais1.populacao);
+    scanf("%lu", &pais1.populacao);//Alterado para %lu devido ao tipo da variavel unsigned long int
 
     printf("Digite a area da cidade em KM quadrados '.'Nao usar para separar centenas:: \n");
     scanf("%f", &pais1.area);
@@ -60,7 +60,7 @@ int main() {
     fgets(pais2.cidade, sizeof(pais2.cidade), stdin);
 
     printf("digite o numero de habitantes da cidade '.'Nao usar para separar centenas: \n");
-    scanf("%d", &pais2.populacao);
+    scanf("%lu", &pais2.populacao);//Alterado para %lu devido ao tipo da variavel unsigned long int
 
     printf("Digite a area da cidade em KM quadrados '.'Nao usar para separar centenas: \n");
     scanf("%f", &pais2.area);
@@ -78,6 +78,15 @@ int main() {
     float densidadePopulacional2 = pais2.populacao / pais2.area;
     float pibPerCapita2 = pais2.pib / pais2.populacao;
 
+    //Calcular "Super Poder" somando todos os atributos numéricos,incluindo inverso da densidade
+    float superpoder1 = pais1.populacao + pais1.area + pais1.pib + pais1.pontosTuristicos
+    + pibPerCapita1 + (1 / densidadePopulacional1);/* (1 / densidadePopulacional1) adiciona o
+    inverso da densidade populacional ao Super Poder. Quanto quanto menor a densidade populacional,
+    maior será o "Super Poder" do país.*/
+
+    float superpoder2 = pais2.populacao + pais2.area + pais2.pib + pais2.pontosTuristicos
+    + pibPerCapita1 + (1 / densidadePopulacional2);
+
     printf("\n"); // Linha em branco para separar as informações
 
     // Imprimindo informações sobre o primeiro pais
@@ -85,24 +94,57 @@ int main() {
     printf("Nome do Estado: %s", pais1.estado);
     printf("Codigo do Estado: %s", pais1.codigo);
     printf("Nome da Cidade: %s", pais1.cidade);
-    printf("Numero de Habitantes da Cidade: %d \n", pais1.populacao);
+    printf("Numero de Habitantes da Cidade: %lu \n", pais1.populacao);//Alterado para %lu
     printf("Tamanho da Area: %.2f \n", pais1.area);
     printf("Pib da Cidade: %.2f \n", pais1.pib);
     printf("Numero de Pontos Turisticos: %d \n", pais1.pontosTuristicos);
     printf("Densidade Populacional: %.2f habitantes por km2 \n", densidadePopulacional1);
-    printf("PIB per Capita: %.2f \n\n", pibPerCapita1);
+    printf("PIB per Capita: %.2f \n", pibPerCapita1);
+    printf("Super Poder 1: %.2f \n\n",superpoder1);
 
     // Imprimindo informações sobre o segundo pais
     printf("Informacoes do Segundo Pais: \n\n");
     printf("Nome do Estado: %s", pais2.estado);
     printf("Codigo do Estado: %s", pais2.codigo);
     printf("Nome da Cidade: %s", pais2.cidade);
-    printf("Numero de Habitantes da Cidade: %.d \n", pais2.populacao);
+    printf("Numero de Habitantes da Cidade: %lu \n", pais2.populacao);//Alterado para %lu
     printf("Tamanho da Area: %.2f \n", pais2.area);
     printf("Pib da Cidade: %2.f \n", pais2.pib);
     printf("Numero de Pontos Turisticos: %d \n", pais2.pontosTuristicos);
     printf("Densidade Populacional: %.2f habitantes por km2 \n", densidadePopulacional2);
     printf("PIB per Capita: %.2f \n", pibPerCapita2);
+    printf("Super Poder 2: %.2f \n\n",superpoder2);
+
+    // Comparação de cada atributo e exibição do vencedor
+    printf("Comparando Pais1 e Pais2 (eles são como cartas):\n\n");
+                                            
+    /* 0 (Pais 1) 1 (Pais 2)
+    Se pais1 > pais2 for verdadeiro, o valor impresso será 1.
+    Se o valor de pais1 < pais2, o valor impresso será 0*/                                        
+    
+    printf("1. Comparando Populacao (): %lu (Pais 1) -%lu (Pais 2)\n", pais1.populacao > pais2.populacao,
+    pais1.populacao < pais2.populacao);
+    
+    printf("2. Comparando Area: %d (Pais 1) -%d (Pais 2)\n", pais1.area > pais2.area,
+    pais1.area < pais2.area);
+    
+    printf("3. Comparando PIB: %d (Pais 1) -%d (Pais 2)\n", pais1.pib > pais2.pib,
+    pais1.pib < pais2.pib);
+    
+    printf("4. Comparando Pontos Turisticos: %d (Pais 1) -%d (Pais 2)\n",
+    pais1.pontosTuristicos > pais2.pontosTuristicos, pais1.pontosTuristicos < pais2.pontosTuristicos);
+    
+    printf("5. Comparando PIB per Capita: %d (Pais 1) -%d (Pais 2)\n", pibPerCapita1 > pibPerCapita2,
+    pibPerCapita1 < pibPerCapita2);
+    
+    printf("6. Comparando Densidade Populacional (menor vence): %d (Pais 1) -%d (Pais 2)\n",
+    densidadePopulacional1 < densidadePopulacional2,densidadePopulacional1 > densidadePopulacional2);
+    
+    printf("7. Comparando Super Poder: %d (Pais 1) -%d (Pais 2)\n", superPoder1 > superPoder2, 
+    superPoder1 < superPoder2);
+
+
+
 
     return 0;
 }
